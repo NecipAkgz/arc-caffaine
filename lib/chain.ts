28dc9cb1ac10bfbdd63d634ee8e8ba4199da77c0
Necip Akgoz <nexco@hotmail.com>
2025-12-31 10:32:18 +0300

    feat: add missing arc icon

--- lib/chain.ts
+++ lib/chain.ts
@@ -1,19 +1,20 @@
-import { defineChain } from 'viem'
+import { defineChain } from "viem";
 
 export const arcTestnet = defineChain({
   id: 5042002,
-  name: 'Arc Testnet',
+  name: "Arc Testnet",
   nativeCurrency: {
     decimals: 18,
-    name: 'USDC',
-    symbol: 'USDC',
+    name: "USDC",
+    symbol: "USDC",
   },
   rpcUrls: {
     default: {
-      http: ['https://rpc.testnet.arc.network'],
+      http: ["https://rpc.testnet.arc.network"],
     },
   },
   blockExplorers: {
-    default: { name: 'ArcScan', url: 'https://testnet.arcscan.app' },
+    default: { name: "ArcScan", url: "https://testnet.arcscan.app" },
   },
-})
+  iconUrl: "/arc-icon.png",
+});
